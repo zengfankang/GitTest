e0c532c537c3ec5d5257c763809fe60bd1bac5ae
Bacteria <3250835449@qq.com>
2022-02-17 18:30:28 +0800

    first commit2

--- src/main/java/com/zeng/Test/GitTest01.java
+++ src/main/java/com/zeng/Test/GitTest01.java
@@ -7,5 +7,6 @@ package com.zeng.Test;
 public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Push Git!");
     }
 }
