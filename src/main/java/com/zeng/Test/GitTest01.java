1f3ea58d343c4f7d40bed94eb70a21d0e44d7ab1
Bacteria <3250835449@qq.com>
2022-02-17 17:18:45 +0800

    first commit

---
+++ src/main/java/com/zeng/Test/GitTest01.java
@@ -0,0 +1,11 @@
+package com.zeng.Test;
+
+/**
+ * @author Bacteria
+ * @date 2022/2/17 - 17:15
+ */
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
