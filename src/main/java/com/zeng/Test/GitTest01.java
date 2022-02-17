18392c23f5612a37d47a9e5dcd178a9a0fe6049d
zengfankang <89532018+zengfankang@users.noreply.github.com>
2022-02-17 18:42:39 +0800

    Update GitTest01.java
    pull commit

--- src/main/java/com/zeng/Test/GitTest01.java
+++ src/main/java/com/zeng/Test/GitTest01.java
@@ -8,5 +8,6 @@ public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Push Git!");
+        System.out.println("Pull Git!");
     }
 }
